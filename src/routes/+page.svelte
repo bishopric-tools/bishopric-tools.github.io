2742f4db5044c4bde6157e1adb64354e2b85ec6c
joshtune <joshua.tune@gmail.com>
2023-08-12 23:01:47 -0600

    init: Convert to static build

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<picture>
+		<source srcset={welcome} type="image/webp" />
+		<img src={welcome_fallback} alt="Welcome" />
+	</picture>
+</section>
