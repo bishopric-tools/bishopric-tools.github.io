0365f91a7258206f72ec79306ef45a0caf48cade
joshtune <joshua.tune@gmail.com>
2024-01-08 19:50:29 -0700

    auto deployment updates

--- app/immutable/chunks/stores.ebc112c1.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.b3d0c74a.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
